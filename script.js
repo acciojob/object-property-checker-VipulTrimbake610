c5eb78662b3a360f7c61f518fbd4b797cc3e38ee
Vipul Trimbake <130178066+VipulTrimbake610@users.noreply.github.com>
2023-06-13 04:17:05 -0700

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for i in sampleObject{
+		console.log(i);
+	}	
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
